29f8f588f6a037bc9add9011f034b09e95fb7c46
Hannah HAmmonds <hannahammonds@gmail.com>
2022-03-14 20:26:36 -0500

    Updated CRUD actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,37 @@ class RecipesController < ApplicationController
         @recipes = Recipe.all 
     end
 
+    def new
+        @recipe = Recipe.new 
+    end
+
+    def create 
+        @recipe = Recipe.new(params.require(:recipe), permit(:name, :ingredients, :instructions))
+        if @recipe.save 
+            flash[:notice] = "Recipe saved successfully!"
+            redirect_to @recipe
+        else 
+            render 'new'
+        end
+    
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update 
+        @recipe = Recipe.find(params[:id])
+        if @book.update(params.require(:recipe).permit(:name, :ingredients, :instructions))
+            flash[:message] = "Update successful!"
+            redirect_to @recipe 
+        else
+            render 'edit'
+    end
+
+    def destroy
+        @book = Book.find(params[:id])
+        flash[:message] = "#{@recipe.name} was deleted successfully!"
+        @recipe.destroy
+        redirect_to books_path 
+    end
+
 end
