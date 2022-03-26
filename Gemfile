644636f158d725f7d321ef5d621bc3c8be5494a5
Hannah HAmmonds <hannahammonds@gmail.com>
2022-03-26 16:16:33 -0500

    image path working

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'open-uri'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
@@ -57,6 +57,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 group :production do 
   gem 'pg'
 end
-
-# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,15 @@ class Recipe < ApplicationRecord
     validates :name, presence:true, length: {minimum: 3, maximum: 60} 
     validates :ingredients, presence:true, length: {minimum: 6, maximum: 100}
     validates :instructions, presence:true, length: {minimum: 6}
-    
+    validate :image_path_exists,
+
+    def image_path_exists
+        require 'open-uri'
+        begin
+            url = URI.open(image_path)
+            errors.add(:image_path, "does not contain image") and return unless url.content_type.starts_with("image")
+        rescue
+            errors.add(:image_path, "Invalid URL")
+        end
+    end
 end
