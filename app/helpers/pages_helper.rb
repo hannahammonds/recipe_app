50e691c762b9fe62eb39395adbc990cd3ac39463
Hannah HAmmonds <hannahammonds@gmail.com>
2022-03-26 16:51:59 -0500

    added random recipes on home page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,11 @@
 module PagesHelper
+    def get_eight_recipes
+        if Recipe.all.count == 0
+            return []
+        elseif Recipe.all.count <9
+            return Recipe.all.count.times.map{Recipe.all[Random.rand(Recipe.count)]}
+        else
+            return 8.times.map {Recipe.all[Random.rand(Recipe.count)]}
+        end
+    end
 end
