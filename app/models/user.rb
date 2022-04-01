45b2a81f855c4f014284912d9e22c9d35dd1ebaf
Hannah HAmmonds <hannahammonds@gmail.com>
2022-03-31 19:16:46 -0500

    add password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_secure_password 
     has_many :recipes, dependent: :destroy
     validates :username, uniqueness: true, presence: true, length: { minimum: 3, maximum: 25 }
     validates :email, uniqueness: true, presence: true, length: { minimum: 5, maximum: 25 }, format: { with: URI::MailTo::EMAIL_REGEXP }
