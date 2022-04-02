b808fc9f88c0a191b1e2060a4075a0e2aaa97ce6
Hannah HAmmonds <hannahammonds@gmail.com>
2022-04-02 11:10:35 -0500

    authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController 
+    before_action :set_user, only: [:show, :edit, :update]
+    
     def index 
         @users = User.all
     end
 
     def show 
-        @user = User.find(params[:id])
+        
     end
 
     def new 
@@ -14,6 +16,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save 
+            session[:user_id] = @user.id 
             flash[:notice] = "Welcome #{@user.username} to Seasonal Plate, you have successfully signed up!"
             redirect_to recipes_path 
         else
@@ -22,7 +25,7 @@ class UsersController < ApplicationController
     end
 
     def edit 
-        @user = User.find(params[:id]) 
+        
     end
 
     def update 
@@ -30,9 +33,13 @@ class UsersController < ApplicationController
     end
 
     private
-    def user_params 
-        params.require(:user).permit(:username, :email, :password) 
-    end
+        def user_params 
+            params.require(:user).permit(:username, :email, :password) 
+        end
+
+        def set_user 
+            @user = User.find(params[:id])
+        end
 end
 
 
