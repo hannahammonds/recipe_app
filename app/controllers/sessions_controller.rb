5c9331bd6d2998501be314f3de8a5e17944c3457
Hannah HAmmonds <hannahammonds@gmail.com>
2022-04-05 21:30:04 -0500

    added cdn link to fix dropdown

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
 
     def create 
         user = User.find_by(email: params[:sessions][:email])
+     
         if user && user.authenticate(params[:sessions][:password])
+
             session[:user_id] = user.id 
             flash[:notice] = "Logged in successfully!"
             redirect_to user
