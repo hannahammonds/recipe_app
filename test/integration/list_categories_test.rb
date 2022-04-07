744bd764cbcdd2db7565b54a3ab3e67b8998d60b
Hannah HAmmonds <hannahammonds@gmail.com>
2022-04-07 18:36:46 -0500

    integration test list categories

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @category1 = Category.create(name: "Summer")
+    @category2 = Category.create(name: "Fall")
+  end
+
+  test "should show cateogry list" do
+    get '/categories'
+    assert_select "a[href=?]", category_path(@category1), text:@category1.name
+    assert_select "a[href=?]", category_path(@category2), text:@category2.name
+  end
 end
