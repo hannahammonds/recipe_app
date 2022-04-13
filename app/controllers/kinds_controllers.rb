2c07ae1c39ddfcdff3c808e8fc7d05da537f0bb4
Hannah HAmmonds <hannahammonds@gmail.com>
2022-04-13 17:27:25 -0500

    kinds controllers test

--- app/controllers/kinds_controllers.rb
+++ app/controllers/kinds_controllers.rb
@@ -1,1 +1,31 @@
-class KindsController < ApplicationController
+class KindsController < ApplicationController 
+    
+    def index 
+
+    end
+
+    def show 
+
+    end
+
+    def new 
+        @kind = Kind.new;
+    end
+
+    def create 
+        @kind = Kind.new(kind_params)
+        if @kind.save 
+            flash[:notice] = "Recipe type was created successfully!" 
+            redirect_to @kind 
+        else
+            flash[:alert] = "Recipe type was not created successfully!"
+            render 'new'
+        end
+    end
+
+    private 
+    def kind_params 
+        params.require(:kind).permit(:name)
+    end
+
+end
---
+++ test/controllers/kinds_controller_test.rb
@@ -0,0 +1,49 @@
+require "test_helper"
+
+class KindsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @kind = Kind.create(name: "Appetizer") 
+
+  end
+
+  test "should get index" do
+    get kinds_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_kind_url
+    assert_response :success
+  end
+
+  test "should create kind" do
+    assert_difference('Kind.count', 1) do
+      post kinds_url, params: { kind: { name: "Soups" } }
+    end
+
+    assert_redirected_to kind_url(Kind.last)
+  end
+
+  test "should show kind" do
+    get kind_url(@kind)
+    assert_response :success
+  end
+
+  # =begin test "should get edit" do
+  #   get edit_kind_url(@kind)
+  #   assert_response :success
+  # end
+
+  # test "should update kind" do
+  #   patch kind_url(@kind), params: { kind: {  } }
+  #   assert_redirected_to kind_url(@kind)
+  # end
+
+  # test "should destroy kind" do
+  #   assert_difference('Kind.count', -1) do
+  #     delete kind_url(@kind)
+  #   end
+
+  #   assert_redirected_to kinds_url
+  # end =end
+end
