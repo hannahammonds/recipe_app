58234349235a994729cea4badef5ef169bf313c1
Hannah HAmmonds <hannahammonds@gmail.com>
2022-04-14 19:09:19 -0500

    styling updates

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,16 +2,17 @@ class Category < ApplicationRecord
     has_many :recipe_categories 
     has_many :recipes, through: :recipe_categories 
     validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximum: 25 }
-    validate :image_path_exists
+    # validate :image_path_exists
     
-    def image_path_exists
-        require 'open-uri'
-        begin
-            url = URI.open(image_path)
-            errors.add(:image_path, "does not contain image") and return unless url.content_type.starts_with?("image")
-        rescue
-            errors.add(:image_path, "Invalid URL")
-        end
-    end
+    # def image_path_exists
+    #     require 'open-uri'
+    #     begin
+    #         url = URI.open(image_path)
+    #         errors.add(:image_path, "does not contain image") and return unless url.content_type.starts_with?("image")
+    #     rescue
+    #         errors.add(:image_path, "Invalid URL")
+    #     end
+    # end
 
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'faker'
-images = [
-    "https://tse4.mm.bing.net/th?id=OIP.Q8DkndBzfTOhULcFiKn2NQHaFD&pid=Api&P=0&w=267&h=182",
-    "https://tse4.mm.bing.net/th?id=OIP.ay2rwpFv_9BCxqqjze-QkwHaE8&pid=Api&P=0&w=274&h=182",
-    "https://tse3.mm.bing.net/th?id=OIP.hrtc1-lKzGnxfqA3SeA0_gHaDJ&pid=Api&P=0&w=427&h=181",
-    "https://tse2.explicit.bing.net/th?id=OIP.BC7ArCQQZ7CKSgSC0qi6rAHaD4&pid=Api&P=0&w=302&h=158",
-]
-(1..100).each do |i| 
-    Recipe.create(name: Faker::Food.dish, ingredients: Faker::Food.ingredient, instructions: Faker::Food.description, image_path: images[Random.rand(4)])
-end 
+# require 'faker'
+# images = [
+#     "https://tse4.mm.bing.net/th?id=OIP.Q8DkndBzfTOhULcFiKn2NQHaFD&pid=Api&P=0&w=267&h=182",
+#     "https://tse4.mm.bing.net/th?id=OIP.ay2rwpFv_9BCxqqjze-QkwHaE8&pid=Api&P=0&w=274&h=182",
+#     "https://tse3.mm.bing.net/th?id=OIP.hrtc1-lKzGnxfqA3SeA0_gHaDJ&pid=Api&P=0&w=427&h=181",
+#     "https://tse2.explicit.bing.net/th?id=OIP.BC7ArCQQZ7CKSgSC0qi6rAHaD4&pid=Api&P=0&w=302&h=158",
+# ]
+# (1..100).each do |i| 
+#     Recipe.create(name: Faker::Food.dish, ingredients: Faker::Food.ingredient, instructions: Faker::Food.description, image_path: images[Random.rand(4)])
+# end 
+
+#grab category path and set it = to the picture file 
+
+Category.all[6].update(image_path: "spring.jpeg") 
+Category.all[7].update(image_path: "summer 2.jpeg")
+Category.all[8].update(image_path: "fall2.jpeg")
+Category.all[9].update(image_path: "winter.jpeg")
